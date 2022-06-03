e9241ddbbf4a4140685c2270dcba5d64b8be0479
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2022-06-03 14:14:52 +0300

    added social media icons and some mobile styling

--- components/layout.js
+++ components/layout.js
@@ -11,29 +11,6 @@ export default function Layout({ children }) {
             <main>
                 {children}
             </main>
-            {/* <footer className="section-footer">
-                <div className="cont">
-                    <div>
-                        <h2 className="lighter">Connect with us on social media.</h2>
-                        <a href="http://twitter.com">
-                            <i className="fa-brands fa-twitter fa-2x"></i>
-                        </a>
-                        <a href="http://facebook.com">
-                            <i className="fab fa-facebook fa-2x"></i>
-                        </a>
-                        <a href="http://instagram.com">
-                            <i className="fa-brands fa-instagram fa-2x"></i>
-                        </a>
-                    </div>
-                    <div>
-                        <h2>Need Some Inspo?</h2>
-                        <a href="https://www.webmd.com/fitness-exercise/ss/twelve-habits-super-healthy-people">Check out this article from WebMD.</a>
-                        <br />
-                        <a href="https://www.psychologytoday.com/us/blog/lifes-work/202103/7-simple-habits-protect-your-mental-health">Or this one from Psychology Today.</a>
-                    </div>
-
-                </div>
-            </footer> */}
         </div>
     )
 }
